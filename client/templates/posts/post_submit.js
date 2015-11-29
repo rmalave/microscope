524cf0165ebbaaf768617c15165182c6d343a34a
Rafael Malave <rafomalave@gmail.com>
2015-11-29 13:18:50 -0800

    validate post contents when editing

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function(field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,8 +20,12 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
+
     Meteor.call('postInsert', post, function(error, result) {
-      //display the error to the user and abort
+      // display the error to the user and abort
       if (error)
         return throwError(error.reason);
 
@@ -16,7 +33,9 @@ Template.postSubmit.events({
       if (result.postExists)
         throwError('This link has already been posted');
 
-      Router.go('postPage', post);
+      Router.go('postPage', {
+        _id: result._id
+      });
     });
   }
-})
+});
