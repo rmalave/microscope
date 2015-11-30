2c669bb18479e787613b15dd42279f4531d58c49
Rafael Malave <rafomalave@gmail.com>
2015-11-29 20:16:52 -0800

    Only sync notifications that are relevant to the user.

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -3,30 +3,30 @@ Template.notifications.helpers({
     return Notifications.find({
       userId: Meteor.userId(),
       read: false
-    });
+    })
   },
   notificationCount: function() {
     return Notifications.find({
       userId: Meteor.userId(),
       read: false
-    });
-
-    Template.notificationItem.helpers({
-      notificationPostPath: function() {
-        return Router.routes.postPage.path({
-          _id: this.postId
-        });
-      }
-    });
-
-    Template.notificationItem.events({
-      'click a': function() {
-        Notifications.update(this._id, {
-          $set: {
-            read: true
-          }
-        });
-      }
-    });
+    }).count();
   }
-});
+})
+
+Template.notificationItem.helpers({
+  notificationPostPath: function() {
+    return Router.routes.postPage.path({
+      _id: this.postId
+    })
+  }
+})
+
+Template.notificationItem.events({
+  'click a': function() {
+    Notifications.update(this._id, {
+      $set: {
+        read: true
+      }
+    })
+  }
+})
